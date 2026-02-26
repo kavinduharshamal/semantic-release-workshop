4a00181ed9fee11d56fed294bf576d72162efe8e
Kavindu Perera <khp@tiqri.com>
2026-02-26 18:14:51 +0530

    feat(users): add validation url support

--- src/server.js
+++ src/server.js
@@ -6,4 +6,5 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT} and ready to accept requests.`);
+  console.log(`You can access the API at http://localhost:${PORT}/api`);
 });

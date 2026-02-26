ce8446d491750dc7159a9a4d3296ba2208e81f58
Kavindu Perera <khp@tiqri.com>
2026-02-26 18:12:09 +0530

    fix(users): add validation

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
-const app = require('./app');
+const app = require("./app");
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT} and ready to accept requests.`);
 });
